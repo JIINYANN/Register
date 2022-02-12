5136a66b78646ce4436c27fb0d04a5ae2638833e
JIINYANN <jiinyann1022@gmail.com>
2022-02-12 16:54:09 +0800

    change

--- js/index.js
+++ js/index.js
@@ -8,7 +8,10 @@ $(function(){
     //創建正規表達式對象
     let usernamePatt = /^\w{5,12}$/;
     //使用test方法驗證--僅判斷不符合條件的時候提示用戶結果
-    if(!usernamePatt.test($usernameText)){
+    if($usernameText == ""){
+      $("#error-msg").text("username cannot be empty!").addClass("error-msg");
+       return false;
+    }else if(!usernamePatt.test($usernameText)){
     //提示用戶結果
     $("#error-msg").text("please enter a valid username!").addClass("error-msg");
     return false;
@@ -19,7 +22,10 @@ $(function(){
     //創建正則表達式對象
     let emailPatt = /^\w{2,}@[A-z0-9]+(\.[A-z]{2,5}){1,2}$/;
     //使用test方法驗證是否合法
-    if(!emailPatt.test($emailText)){
+    if($emailText == ""){
+      $("#error-msg").text("Email cannot be empty!").addClass("error-msg");
+       return false;
+    }else if(!emailPatt.test($emailText)){
       //提示用戶
       $("#error-msg").text("Email is not a valid email address!").addClass("error-msg");
      return false;
@@ -30,7 +36,10 @@ $(function(){
      //創建正規表達式對象
      let passwordPatt = /^\w{5,12}$/;
     //使用test方法驗證--僅判斷不符合條件的時候提示用戶結果
-   if(!passwordPatt.test($passwordText)){
+    if($passwordText == ""){
+      $("#error-msg").text("password cannot be empty!").addClass("error-msg");
+       return false;
+    }else if(!passwordPatt.test($passwordText)){
      //提示用戶結果
       $("#error-msg").text("please enter a valid password!").addClass("error-msg");
       return false;
@@ -40,13 +49,14 @@ $(function(){
     //獲取確認密碼內容
     let $confirmpasswordText = $("#confirm-password").val();
     //與密碼相比較
-    if($passwordText != $confirmpasswordText){
+    if($confirmpasswordText == ""){
+      $("#error-msg").text("confirm-password cannot be empty!").addClass("error-msg");
+       return false;
+    }else if($passwordText != $confirmpasswordText){
      // 提示用戶結果
      $("#error-msg").text("please make sure your passwords match!").addClass("error-msg");
      return false;
     }
-
-    
 
     //只要用戶名合法，提示訊息就要刪掉，不要再顯示
     $("#error-msg").text("").removeClass("error-msg");
